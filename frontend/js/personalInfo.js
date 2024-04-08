c8cb8100e3678ad208b8c0ede0916ad1d99dbcfd
Pratham Darji <pratham14104@gmail.com>
2024-04-08 16:14:26 +0530

    Initial Commit

---
+++ frontend/js/personalInfo.js
@@ -0,0 +1,77 @@
+function apiCall(method, url, data) {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          let resData = xhr.responseText;
+          resolve(JSON.parse(resData));
+        } else {
+          reject(xhr.responseText);
+        }
+      }
+    };
+
+    xhr.open(method, url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
+  });
+}
+
+function personalInfo() {
+  let output = document.getElementById("output");
+  let symbol = document.getElementById("div-symbol");
+  apiCall("get", "http://localhost:8000/api/users/userInfo")
+    .then((message) => {
+      if (message.role == "admin") {
+        symbol.innerHTML = `
+        <span class="material-symbols-outlined" id="personInfo">
+        <a href="http://127.0.0.1:5500/frontend/admin-panel.html"> Home </a>
+      </span>
+      <h2>Check Out</h2>
+      <span class="material-symbols-outlined">
+        
+        <a  id="logout" onclick="logout()">logout</a>
+      </span>
+        `;
+      }
+
+      if (message.role == "user") {
+        symbol.innerHTML = `
+        <span class="material-symbols-outlined" id="personInfo">
+          <a href="http://127.0.0.1:5500/frontend/home.html"> Home </a>
+        </span>
+        <h2>Check Out</h2>
+        <span class="material-symbols-outlined">
+        <a href="http://127.0.0.1:5500/frontend/view-cart.html"
+          >shopping_cart</a
+        >
+        <a id="logout" onclick="logout()">logout</a>
+        </span>
+        `;
+      }
+
+      output.innerHTML = `
+      <div class="output__wrapper">
+        <p>username: ${message.name}</p>
+        <p>email: ${message.email}</p>
+        <p>phone: ${message.phone}</p>
+      </div>
+      `;
+    })
+    .catch((err) => {
+      console.log(err);
+      output.innerHTML = `
+        <h1 class="error">Please <a href="http://127.0.0.1:5500/frontend/login.html">Login</a> To access this page</h1>`;
+    });
+}
+
+personalInfo();
+
+async function logout() {
+  let logout = document.getElementById("logout");
+
+  await apiCall("get", "http://localhost:8000/api/users/logout");
+  window.location.href = "http://127.0.0.1:5500/frontend/login.html";
+}
---
+++ frontend/js/view-cart.js
@@ -0,0 +1,106 @@
+function apiCall(method, url, data) {
+  return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200) {
+          let resData = xhr.responseText;
+          resolve(JSON.parse(resData));
+        } else {
+          reject(xhr.responseText);
+        }
+      }
+    };
+
+    xhr.open(method, url, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
+  });
+}
+
+async function viewCart() {
+  let allData = [];
+  let selectedId = [];
+  let output = document.getElementById("output");
+  await apiCall("get", "http://localhost:8000/api/users/viewCart")
+    .then((message) => {
+      message.forEach((e) => {
+        selectedId.push(e);
+      });
+
+      // console.log(selectedId);
+    })
+    .catch((err) => {
+      console.log(err);
+      output.innerHTML = `
+        <h1>Please <a href="http://127.0.0.1:5500/frontend/login.html">Login</a> To access this page</h1> `;
+    });
+
+  await apiCall("get", "http://localhost:8000/api/product/data", {})
+    .then((message) => {
+      message.forEach((e) => {
+        allData.push(e);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      output.innerHTML = `
+        <h1>Please <a href="http://127.0.0.1:5500/frontend/login.html">Login</a> To access this page</h1> `;
+    });
+
+  if (selectedId == 0) {
+    output.innerHTML = `
+    <img src="./images/empty-cart.png" alt="empty-cart image">
+    `;
+  } else {
+    selectedId.forEach((e) => {
+      let productId = e.product.productId;
+      let qty = e.product.Qty;
+      allData.forEach((j) => {
+        if (j._id == productId) {
+          // console.log(e);
+          output.innerHTML += `
+              <div class="card__wrapper">
+                  <img src="${j.imageUrl}">
+                  <span><b>Product: </b>${j.name}</span>
+                  <span><b>Type: </b>${j.type}</span>
+                  <p><b>Description:</b> ${j.description}</p>
+                  <div class="price-qty">
+                    <p><b>Price:</b> ₹${j.price} </p>
+                    <p><b>Qty:</b> ${qty}</p>
+                  </div>
+                  
+                  <div class="buttons">
+                  <button class="addBtn" id="payBtn">Pay Total ₹${
+                    j.price * qty
+                  }</button>
+                  <button class="addBtn" id="dltBtn" onclick="deleteFunc('${productId}')">Delete Product</button>
+                  </div>
+              </div>
+            `;
+        }
+      });
+    });
+  }
+}
+
+viewCart();
+
+async function deleteFunc(id) {
+  let data = { productId: id };
+  // console.log(data);
+  await apiCall("post", "http://localhost:8000/api/users/deleteQty", data);
+  window.location.reload();
+}
+
+function logout() {
+  let logout = document.getElementById("logout");
+
+  logout.addEventListener("click", async function () {
+    await apiCall("get", "http://localhost:8000/api/users/logout");
+    window.location.href = "http://127.0.0.1:5500/frontend/login.html";
+  });
+}
+
+logout();
